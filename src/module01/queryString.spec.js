c5d1244ec1a6628d5c7d9f8eb49d1e1a04b146db
George Bezerra <hi@georgebezerra.dev>
2023-12-12 16:43:15 -0300

    initial queryString

---
+++ src/module01/queryString.spec.js
@@ -0,0 +1,19 @@
+const { queryString } = require('./queryString')
+describe('Object to query string', () => {
+    it('should  create a valid query string when an object is provitect', function () {
+        const obj = {
+            name: 'George',
+            profession: 'developer'
+        };
+
+        queryString(obj);
+
+        expect(queryString(obj)).toBe(
+            'name=George&profession=developer'
+        );
+    });
+});
+
+// describe('Query string to object', () => {
+//
+// });
