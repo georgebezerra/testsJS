fd2a043bcf894c968098f1468d94eb75245808bb
George Bezerra <hi@georgebezerra.dev>
2023-12-12 10:48:19 -0300

    add TDD add --watchAll parseInt .toThrowError() and .not.toThrowError()

--- src/module01/calculator.spec.js
+++ src/module01/calculator.spec.js
@@ -1,9 +1,36 @@
 const { sum } = require('./calculator')
+const { sumFALSEPOSITIVE } = require('./calculator')
 
 it('should sum 2 and 2 and the result must be 4', function () {
     expect(sum(2,2)).toBe(4);
 });
 
-it('TESTANDO O FALSO POSITIVO', function () {
-    expect(sum(2,2)).not.toBe(4);
-});
+// it('TESTANDO O FALSO POSITIVO', function () {
+//     expect(sumFALSEPOSITIVE(2,2)).not.toBe(4);
+// });
+// it('should  sum 2 and 2 even if one of them is a string and the result must be 4', function () {
+//     expect(sum('2', '2')).toBe(4);
+// });
+
+//assinatura para capturar o ERRO quando o método for disparado.
+it('should throw an error if what is provided to the method cannot be summed', function () {
+    expect(() => {
+        sum('', '2')
+    }).toThrowError()
+
+    expect(() => {
+        sum([2,2])
+    }).toThrowError()
+
+    // expect(() => {
+    //     sum([2,2])
+    // }).not.toThrowError()
+
+    expect(() => {
+        sum({})
+    }).toThrowError()
+
+    expect(() => {
+        sum()
+    }).toThrowError()
+}); 
