ac108778fc4bc062543349afd3b976bffa2d72a0
George Bezerra <hi@georgebezerra.dev>
2023-12-13 14:16:22 -0300

    Object to query string

--- src/module01/queryString.js
+++ src/module01/queryString.js
@@ -1,13 +1,51 @@
-module.exports.queryString = obj => {
-  console.log(Object.entries(obj))
+//SEM REFATORAR
+// module.exports.queryString = obj => {
+//   console.log(Object.entries(obj));
+//
+//   const entries = Object.entries(obj).map(item => {
+//     console.log(item);
+//
+//     return `${item[0]}=${item[1]}`;
+//   });
+//
+//   console.log(entries.join('&'));
+//
+//   return entries.join('&');
+// };
 
-  const entries = Object.entries(obj).map(item => {
-    console.log(item)
+//COM REFATORAÇÃO
+// module.exports.queryString = obj => {
+//   return Object.entries(obj)
+//     .map(item => {
+//       return `${item[0]}=${item[1]}`;
+//     })
+//     .join('&');
+// };
 
-    return `${item[0]}=${item[1]}`
-  })
+//MAIS REFATORAÇÃO
+// module.exports.queryString = obj =>
+//   Object.entries(obj)
+//     .map(([key, value]) => `${key}=${value}`)
+//     .join('&');
 
-  console.log(entries.join('&'))
+//ESTRESSANDO A IMPLEMENTAÇÃO PRA TORNA-LA RESILIENTE
+// module.exports.queryString = obj =>
+//   Object.entries(obj)
+//     .map(([key, value]) => {
+//       if (typeof value === 'object' && !Array.isArray(value)) {
+//         throw new Error('Please check you params');
+//       }
+//       return `${key}=${value}`;
+//     })
+//     .join('&');
 
-  return entries.join('&')
-}
+//REFATORANDO - ESTRESSANDO A IMPLEMENTAÇÃO PRA TORNA-LA RESILIENTE
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Please check you params');
+  }
+  return `${key}=${value}`;
+};
+
+module.exports.queryString = obj =>
+  Object.entries(obj).map(keyValueToString).join('&');
--- src/module01/queryString.spec.js
+++ src/module01/queryString.spec.js
@@ -1,16 +1,41 @@
-const { queryString } = require('./queryString')
+const { queryString } = require('./queryString');
+
 describe('Object to query string', () => {
-  it('should  create a valid query string when an object is provitect', function () {
+  it('should  create a valid query string when an object is provided', () => {
     const obj = {
       name: 'George',
       profession: 'developer',
-    }
+    };
 
-    queryString(obj)
+    queryString(obj);
+    expect(queryString(obj)).toBe('name=George&profession=developer');
+  });
 
-    expect(queryString(obj)).toBe('name=George&profession=developer')
-  })
-})
+  it('should  create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'George',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=George&abilities=JS,TDD');
+  });
+
+  it('should throw an error when an object is passed a value', () => {
+    const obj = {
+      name: 'George',
+      abilites: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    console.log(queryString(obj));
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
+});
 
 // describe('Query string to object', () => {
 //
